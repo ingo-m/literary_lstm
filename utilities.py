74a1d0007497aa1fbf9b162ec002ce838a551422
John <ingo.marquardt@gmx.de>
2019-04-30 14:05:23 +0200

    Rewriting batch-generating function with Gaussian sampling of context words - wip.

--- utilities.py
+++ utilities.py
@@ -238,3 +238,125 @@ def generate_batch(lstC, glbVarIdx, varBatSze=8, varNumSkp=2, varConWin=1):
             glbVarIdx = (glbVarIdx + len(lstC) - varSpan) % len(lstC)
 
     return vecWrds, aryCntxt, glbVarIdx
+
+
+def generate_batch_2(lstC, varIdx, varBatSze=5, varConWin=5.0, varTrnk=10):
+    """
+    Generate training batch for skip-gram model.
+
+    Inputs
+    ------
+    lstC : list
+        Coded version of original text (corpus), where words are coded as
+        integers. The integer code of a word is its ordinal occurence
+        number (i.e. the 50th most common word has the code 50).
+    varIdx: int
+        Position (index) within corpus where to start sampling.
+    varBatSze : int
+        Batch size (number of words).
+    varConWin : float
+        Standard deviation of the context window (i.e. number of words to
+        consider to the left and to the right of the target word). In order to
+        more frequently sample nearby words, we base the selection of context
+        words on a Gaussian distribution.
+    varTrnk : int
+        Cutoff for truncation of context word window. Context words are sampled
+        from a Gaussian distrubution around the sample word, so that nearby
+        words are sampled with higher probability. However, the probability
+        distribution needs to be truncated so as not to run over the limits of
+        the corpus.
+
+    Returns
+    -------
+    vecWrds : np.array
+        Batch of (integer) codes of input words (whose context to predict).
+    aryCntxt : np.array
+        Vector (n*1) of context words (corresponding to the input words) to
+        predict.
+    varIdx: int
+        ???
+
+    Notes
+    -----
+    ???
+    """
+    # Batch of (integer) codes of input words (whose context to predict).
+    vecWrds = np.zeros(varBatSze, dtype=np.int32)
+
+    # labels = np.ndarray(shape=(varBatSze, 1), dtype=np.int32)
+    aryCntxt = np.zeros((varBatSze, 1), dtype=np.int32)
+
+a = 0
+
+varBatSze = 10000
+vecConWin = 5
+varTrnk=10
+
+    # Vector with indices of context words, relative to sample word (e.g. -1
+    # would refer to word before sample word, +1 to the word after the sample
+    # word. We start with a Gaussian distribution (float), which will
+    # subsequently be rounded to integer indices. Negative values are rounded
+    # to floor, positive values to ceiling, so there will be no zeros (zero =
+    # index of sample word).
+    vecRndn = np.multiply(np.random.randn(varBatSze), varConWin)
+    # Which indices are negative?
+    vecLgcNgt = np.less(vecRndn, 0.0)
+    vecLgcPst = np.logical_not(vecLgcNgt)
+    # Negative indices are rounded to the floor:
+    vecRndn[vecLgcNgt] = np.floor(vecRndn[vecLgcNgt])
+    # Positive indices are rounded to ceiling:
+    vecRndn[vecLgcPst] = np.ceil(vecRndn[vecLgcPst])
+    # Cast indices to integer:
+    vecRndn = vecRndn.astype(np.int32)
+
+aaa = np.equal(vecRndn, 0)
+np.sum(aaa)
+
+
+
+b = 2
+
+    # Increment index:
+    # varIdx += varSpan
+
+    # Loop through batch:
+    for idx01 in range(varBatSze):
+
+        # Sample word for which context should be predicted into vector:
+        vecWrds[idx01] = lstC[varIdx]
+
+        #
+
+        context_words = [w for w in range(varSpan) if w != varConWin]
+
+        words_to_use = random.sample(context_words, varNumSkp)
+
+        # print("context_words: " + str([dictRvrs[x] for x in context_words]))
+        # print("words_to_use: " + str([dictRvrs[x] for x in words_to_use]))
+
+        for idx02, context_word in enumerate(words_to_use):
+
+            # print("idx02 = " + str(idx02))
+            # print("context_word: " + dictRvrs[context_word])
+
+            vecWrds[idx01 * varNumSkp + idx02] = objBuf[varConWin]
+
+            aryCntxt[idx01 * varNumSkp + idx02, 0] = objBuf[context_word]
+
+            # print("vecWrds: " + dictRvrs[vecWrds[idx01 * varNumSkp + idx02]])
+            # print("aryCntxt: " + dictRvrs[[idx01 * varNumSkp + idx02, 0][0]])
+
+            if glbVarIdx == len(lstC):
+                objBuf.extend(lstC[0:varSpan])
+                glbVarIdx = varSpan
+
+            else:
+                # print("glbVarIdx = " + dictRvrs[lstC[glbVarIdx]])
+                objBuf.append(lstC[glbVarIdx])
+                glbVarIdx += 1
+
+            # Backtrack a little bit to avoid skipping words in the end of a
+            # batch
+            glbVarIdx = (glbVarIdx + len(lstC) - varSpan) % len(lstC)
+
+#    return vecWrds, aryCntxt, glbVarIdx
