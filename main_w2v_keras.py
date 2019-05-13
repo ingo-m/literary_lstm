1d27f8ab1202b239eb6f31569e65f83df7d33526
Ingo Marquardt <ingo.marquardt@gmx.de>
2019-05-13 15:51:07 +0200

    Create batches in separate thread - wip.

--- main_w2v_keras.py
+++ main_w2v_keras.py
@@ -5,6 +5,7 @@
 import os
 import random
 import datetime
+import threading
 import numpy as np
 import tensorflow as tf
 from utilities import read_text
@@ -40,6 +41,8 @@ varNrn01 = 100
 # Length of new text to generate:
 varLenNewTxt = 100
 
+# Batch size:
+varSzeBtch = 1000
 
 # -----------------------------------------------------------------------------
 # *** Load data
@@ -72,6 +75,12 @@ aryTfEmb = tf.constant(aryEmb, dtype=tf.float32)
 # -----------------------------------------------------------------------------
 # *** Preparations
 
+# Create tf session:
+objSess = tf.Session()
+
+# Tell keras about tf session:
+tf.keras.backend.set_session(objSess)
+
 # Vocabulary size (number of words):
 varNumWrds = aryEmb.shape[0]
 
@@ -97,12 +106,12 @@ varNrn02 = varSzeEmb
 # varNumInTtl = varNumIn * varSzeEmb
 
 # Placeholder for inputs (to input layer):
-aryWrdsIn = tf.keras.Input(shape=(varNumIn, varSzeEmb),
-                           batch_size=1,
-                           dtype=tf.float32)
+#aryWrdsIn = tf.keras.Input(shape=(varNumIn, varSzeEmb),
+#                           batch_size=1,
+#                           dtype=tf.float32)
 
 # Placeholder for output:
-vecWrdsOut = tf.placeholder(tf.float32, [1, varSzeEmb])
+#vecWrdsOut = tf.placeholder(tf.float32, [1, varSzeEmb])
 
 
 # -----------------------------------------------------------------------------
@@ -118,6 +127,59 @@ vecWrdsOut = tf.placeholder(tf.float32, [1, varSzeEmb])
 #strPthLog = os.path.join(strPthLog, strDate)
 #
 #objCallback = tf.keras.callbacks.TensorBoard(log_dir=strPthLog)
+
+
+
+
+# -----------------------------------------------------------------------------
+# *** Prepare queue
+
+# Batches are prepared in a queue-feeding-function that runs in a separate
+# thread.
+
+# Queue capacity:
+varCapQ = 10
+
+# Queue for batches of context words:
+objQ01 = tf.FIFOQueue(capacity=varCapQ,
+                      dtypes=[tf.float32],
+                      shapes=[(varSzeBtch, varNumIn, varSzeEmb)])
+
+# Queue for batches of target words:
+objQ02 = tf.FIFOQueue(capacity=varCapQ,
+                      dtypes=[tf.float32],
+                      shapes=[(varSzeBtch, varSzeEmb)])
+
+# Method for getting queue size:
+objSzeQ = objQ01.size()
+
+# Placeholder that is used to put batch on computational graph:
+objPlcHld01 = tf.placeholder(tf.float32,
+                             shape=[varSzeBtch, varNumIn, varSzeEmb])
+objPlcHld02 = tf.placeholder(tf.float32,
+                             shape=[varSzeBtch, varSzeEmb])
+
+# The enqueue operation that puts data on the graph.
+objEnQ01 = objQ01.enqueue([objPlcHld01])
+objEnQ02 = objQ02.enqueue([objPlcHld02])
+
+# Number of threads that will be created:
+varNumThrd = 1
+
+# The queue runner (places the enqueue operation on the queue?).
+objRunQ01 = tf.train.QueueRunner(objQ01, [objEnQ01] * varNumThrd)
+tf.train.add_queue_runner(objRunQ01)
+objRunQ02 = tf.train.QueueRunner(objQ02, [objEnQ02] * varNumThrd)
+tf.train.add_queue_runner(objRunQ02)
+
+# The tensor object that is retrieved from the queue. Functions like
+# placeholders for the data in the queue when defining the graph.
+#objCntxt = objQ01.dequeue()
+#objTrgt = objQ02.dequeue()
+objCntxt = tf.keras.Input(shape=(varSzeBtch, varNumIn, varSzeEmb),
+                          tensor=objQ01.dequeue(),
+                          dtype=tf.float32)
+objTrgt = objQ02.dequeue()  # tf.placeholder(tf.float32, [varSzeBtch, varSzeEmb])
 
 
 # -----------------------------------------------------------------------------
@@ -152,7 +214,7 @@ aryOut01 = tf.keras.layers.LSTM(varNrn01,
                                 stateful=True,
                                 unroll=False,
                                 name='LSTMlayer01'
-                                )(aryWrdsIn)
+                                )(objCntxt)
 
 # Second LSTM layer:
 aryOut02 = tf.keras.layers.LSTM(varNrn02,
@@ -187,7 +249,7 @@ aryOut02 = tf.keras.layers.LSTM(varNrn02,
 # objMdl.add(tf.keras.layers.Dense(1))
 
 # Initialise the model:
-objMdl = tf.keras.models.Model(inputs=aryWrdsIn, outputs=aryOut02)
+objMdl = tf.keras.models.Model(inputs=objCntxt, outputs=aryOut02)
 
 # Print model summary:
 objMdl.summary()
@@ -197,8 +259,92 @@ objMdl.compile(optimizer=tf.keras.optimizers.RMSprop(lr=varLrnRte),
                loss=tf.losses.mean_squared_error,
                metrics=['accuracy'])
 
+
 # -----------------------------------------------------------------------------
 # *** Prepare batches
+
+# Batches are prepared in a queue-feeding-function that runs in a separate
+# thread.
+
+def funcPlcIn():
+    """Place data on queue."""
+
+    # Batch index:
+    idx01 = 0    
+    
+    # Array for new batch of context words:
+    aryCntxt = np.zeros((varSzeBtch, varNumIn, varSzeEmb), dtype=np.float32)
+
+    # Array for new batch of target words:
+    aryTrgt = np.zeros((varSzeBtch, varSzeEmb), dtype=np.float32)
+
+    # varNumBtch = int(np.ceil(float(varLenTxt) / float(varSzeBtch)))
+    
+    # Loop through iterations:
+    for idxItr in range(varNumItr):
+    
+          # Loop through text (corpus). Index refers to target word (i.e. word
+        # to be predicted).
+        for idxWrd in range(varNumIn, varLenTxt):
+
+            # Get integer codes of context word(s):
+            vecCntxt = vecC[(idxWrd - varNumIn):idxWrd]
+            
+            # Get embedding vectors for context word(s):
+            aryCntxt[idx01, :, :] = np.array(aryEmb[vecCntxt, :])
+
+            # Word to predict (target):
+            varTrgt = vecC[idxWrd]
+            
+            # Get embedding vector for target word:
+            aryTrgt[idx01, :] = aryEmb[varTrgt, :]            
+
+            # Increment index:
+            idx01 += 1
+
+            # Check whether end of batch has been reached:
+            if idx01 == int(varSzeBtch):
+
+                # TODO
+                
+                aryTmp01 = aryCntxt
+                dicIn01 = {objPlcHld01: aryTmp01}                
+                aryTmp02 = aryTrgt
+                dicIn02 = {objPlcHld02: aryTmp02}                
+
+                # Batch is complete, push to the queue:
+                objSess.run(objEnQ01, feed_dict=dicIn01)
+                objSess.run(objEnQ02, feed_dict=dicIn02)
+
+                # Array for new batch of context words:
+                aryCntxt = np.zeros((varSzeBtch, varNumIn, varSzeEmb),
+                                    dtype=np.float32)
+
+                # Array for new batch of target words:
+                aryTrgt = np.zeros((varSzeBtch, varSzeEmb),
+                                   dtype=np.float32)                
+                
+                # Reset index:
+                idx01 = 0
+
+
+# -----------------------------------------------------------------------------
+# *** Fill queue
+
+# Buffer size (number of samples to put on queue before starting
+# execution of graph):
+varBuff = 10
+
+# Define & run extra thread with graph that places data on queue:
+objThrd = threading.Thread(target=funcPlcIn)
+objThrd.setDaemon(True)
+objThrd.start()
+
+# Stay in this while loop until the specified number of samples
+# (varBuffer) have been placed on the queue).
+varTmpSzeQ = 0
+while varTmpSzeQ < varBuff:
+    varTmpSzeQ = objSess.run(objSzeQ)
 
 
 # -----------------------------------------------------------------------------
@@ -218,29 +364,17 @@ for idxItr in range(varNumItr):
     # to be predicted).
     for idxWrd in range(varNumIn, varLenTxt):
 
-        # Get integer codes of context word(s):
-        vecCntxt = vecC[(idxWrd - varNumIn):idxWrd]
-
-        # Get embedding vectors for words:
-        aryCntxt = np.array(aryEmb[vecCntxt, :]).reshape((1, varNumIn, varSzeEmb))
-
-        # Word to predict (target):
-        varTrgt = vecC[idxWrd]
-
-        # Get embedding vector for target word:
-        vecTrgt = aryEmb[varTrgt, :].reshape((1, varSzeEmb))
-
         # Run optimisation:
         #objMdl.fit(x=aryCntxt,  # run on entire dataset
         #           y=vecTrgt,
         #           verbose=0,
         #           epochs=1)
         #           # callbacks=[objCallback])
-        varLoss01 = objMdl.train_on_batch(aryCntxt,  # run on single batch
-                                          y=vecTrgt)
+        varLoss01 = objMdl.train_on_batch(objCntxt,  # run on single batch
+                                          y=objTrgt)
 
         # Status feedback:
-        if (idxItr % varDspStp == 0) and (idxWrd == varRndm):
+        if False:  # (idxItr % varDspStp == 0) and (idxWrd == varRndm):
 
             try:
 
