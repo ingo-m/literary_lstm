6986c265dd6c67a74a4fe63d7e1e0c2c3389007c
Ingo Marquardt <ingo.marquardt@gmx.de>
2019-05-25 14:15:50 +0200

    Made minimal example more minimal.

--- bug_minimal_example.py
+++ bug_minimal_example.py
@@ -6,37 +6,26 @@ import numpy as np
 import tensorflow as tf
 
 batch_size = 32
-
 sequence_len = 1
-
 embedding_size = 100
 
 x_train = np.random.randn(batch_size, sequence_len, embedding_size)
-
 y_train = np.random.randn(batch_size, embedding_size)
-
 sample_weight = np.random.randn(batch_size)
 
 train_input = tf.keras.Input(shape=(sequence_len, embedding_size),
-                             batch_size=batch_size,
-                             dtype=tf.float32)
+                             batch_size=batch_size)
 
 lstm_layer = tf.keras.layers.LSTM(200,
-                                  # input_shape=(varNumIn, varSzeEmb),
-                                  # batch_size=varSzeBtch,
                                   return_sequences=False,
-                                  stateful=True,
                                   )(train_input)
 
-
 dense_layer = tf.keras.layers.Dense(embedding_size,
-                                    # batch_size=varSzeBtch,
-                                    activation=tf.keras.activations.tanh,
                                     )(lstm_layer)
 
 model = tf.keras.models.Model(inputs=train_input, outputs=dense_layer)
 
-# model.summary()
+model.summary()
 
 model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=0.001),
               loss=tf.losses.mean_squared_error)
