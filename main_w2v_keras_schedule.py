ac685120fd69f976843b1d540451f73403053e4a
Ingo Marquardt <ingo.marquardt@gmx.de>
2019-07-09 21:34:20 +0200

    Training regime with mid-sized batch size (32) and adjusted training window increment (every 50th optimisation step, after 200,000 initial iterations on short text segment).

--- main_w2v_keras_schedule.py
+++ main_w2v_keras_schedule.py
@@ -60,7 +60,7 @@ varNrn05 = 500
 varLenNewTxt = 100
 
 # Batch size:
-varSzeBtch = 1
+varSzeBtch = 32
 
 # Input dropout:
 varInDrp = 0.5
@@ -586,19 +586,21 @@ def training_queue():
                 # length has been reached:
                 varIdxWrd = varNumIn
 
-                # Do not increment training window at the very beginning of
-                # training:
-                if idxOpt > 100000:
-
-                    # Only increase length of training window if end of text
-                    # has not been reached yet:
-                    if varLenTxt > varTrainWin:
-                        print('---')
-                        print('Optimisation step: '
-                              + str(idxOpt)
-                              + ' --- Training window length: '
-                              + str(varTrainWin))
-                        varTrainWin += 1
+        # Do not increment training window at the very beginning of
+        # training:
+        if idxOpt > 200000:
+            # Do not increment training window on every single optimisation
+            # step:
+            if (idxOpt % 50 == 0):
+                # Only increase length of training window if end of text
+                # has not been reached yet:
+                if varLenTxt > varTrainWin:
+                    print('---')
+                    print('Optimisation step: '
+                          + str(idxOpt)
+                          + ' --- Training window length: '
+                          + str(varTrainWin))
+                    varTrainWin += 1
 
         # Put index of next target word on the queue (target word after current
         # batch, because index has already been incremented):
