ad07d8ec0cb85d2d0bb91c5c7eed48f8225c3746
Ingo Marquardt <ingo.marquardt@gmx.de>
2019-08-07 02:54:20 +0200

    Separate validation model can predict if number of iterations to set internal state is large enough. However, dropout is detremential to learning in this simple example.

--- bug_minimal_example_04.py
+++ bug_minimal_example_04.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Test script for finding problem with non-converging LSTM in tf 1.14.0."""
@@ -13,24 +14,23 @@ iterations = 100000
 train_seq = np.array([1.0, 1.0, 1.0,
                       0.0, 0.0, 0.0], dtype=np.float32)
 
-sample_weight = None  # np.array([0.99], dtype=np.float32)
+sample_weight = np.array([0.95], dtype=np.float32)
+
+# Training model
 
 train_input = tf.keras.Input(shape=(1, 1),
                              batch_size=batch_size)
 
-train_tinput = tf.keras.Input(shape=(1, 1),
-                              batch_size=batch_size)
-
-lstm_01 = tf.keras.layers.LSTM(10,
-                               #dropout=0.0,
-                               #recurrent_dropout=0.0,
+lstm_01 = tf.keras.layers.LSTM(100,
+                               dropout=0.4,
+                               recurrent_dropout=0.4,
                                return_sequences=True,
                                stateful=True,
                                )(train_input)
 
-lstm_02 = tf.keras.layers.LSTM(10,
-                               #dropout=0.0,
-                               #recurrent_dropout=0.0,
+lstm_02 = tf.keras.layers.LSTM(100,
+                               dropout=0.4,
+                               recurrent_dropout=0.4,
                                return_sequences=False,
                                stateful=True,
                                )(lstm_01)
@@ -47,29 +47,29 @@ model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.001),
               loss=tf.keras.losses.mean_squared_error)
               #loss=tf.losses.mean_squared_error)
 
-# Validation model:
-lstm_t1 = tf.keras.layers.LSTM(10,
-                               #dropout=0.0,
-                               #recurrent_dropout=0.0,
-                               return_sequences=True,
-                               stateful=True,
-                               )(train_input)
+# Validation model
 
-lstm_t2 = tf.keras.layers.LSTM(10,
-                               #dropout=0.0,
-                               #recurrent_dropout=0.0,
-                               return_sequences=False,
-                               stateful=True,
-                               )(lstm_t1)
+test_input = tf.keras.Input(shape=(1, 1),
+                            batch_size=batch_size)
 
-dense_tlayer = tf.keras.layers.Dense(1,
-                                    )(lstm_t2)
+v_lstm_01 = tf.keras.layers.LSTM(100,
+                                 dropout=0.0,
+                                 recurrent_dropout=0.0,
+                                 return_sequences=True,
+                                 stateful=True,
+                                 )(test_input)
 
-# Whether or not a separate validation model is defined influences generation
-# performance.
+v_lstm_02 = tf.keras.layers.LSTM(100,
+                                 dropout=0.0,
+                                 recurrent_dropout=0.0,
+                                 return_sequences=False,
+                                 stateful=True,
+                                 )(v_lstm_01)
 
-#val_model = tf.keras.models.Model(inputs=train_tinput, outputs=dense_tlayer)
-val_model = tf.keras.models.Model(inputs=train_input, outputs=dense_layer)
+v_dense_layer = tf.keras.layers.Dense(1,
+                                      )(v_lstm_02)
+
+v_model = tf.keras.models.Model(inputs=test_input, outputs=v_dense_layer)
 
 seq_len = train_seq.shape[0]
 idx_x = 0
@@ -94,17 +94,26 @@ for idx_itr in range(iterations):
     if (idx_itr % 10000 == 0):
 
         # Copy weights from training model to test model:
-        val_model.set_weights(model.get_weights())
+        v_model.set_weights(model.get_weights())
+
+        for idx_state in range(1000):
+            x_pred = train_seq[idx_x].reshape(1, 1, 1)
+            _ = v_model.predict_on_batch(x_pred)
+            idx_x += 1
+            if seq_len <= idx_x:
+                idx_x = 0
 
         new_sequence = []
 
         x_pred = train_seq[idx_x].reshape(1, 1, 1)
 
         for idx_new in range(20):
-            y_pred = val_model.predict_on_batch(x_pred)
+            y_pred = v_model.predict_on_batch(x_pred)
             x_pred[0, 0, 0] = y_pred[0, 0]
             new_sequence.append(str(np.around(y_pred[0, 0], decimals=2)))
         print(' '.join(new_sequence))
 
         model.reset_states()
-        val_model.reset_states()
+        v_model.reset_states()
+        idx_x = 0
+        idx_y = 1
