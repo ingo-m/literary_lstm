8782d8e4ec2497b5ea3890d5c1934147ca195207
Ingo Marquardt <ingo.marquardt@gmx.de>
2019-10-18 00:48:38 +0200

    Metascript to call lstm script within tensorflow docker container.

---
+++ tf_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+data_path="/home/john/Dropbox/Harry_Potter/"
+analy_path="/home/john/PhD/GitLab/literary_lstm/"
+
+docker run --gpus all -it --rm \
+    -v ${analy_path}:${analy_path} \
+    -v ${data_path}:${data_path} \
+    -u $(id -u):$(id -g) \
+    tensorflow/tensorflow:1.14.0-gpu-py3 python ${analy_path}main_w2v_keras_oneloss.py
