5ad8cf5f1bfddbef5e099d1754f5efe15c1c52f9
Ingo Marquardt <ingo.marquardt@gmx.de>
2019-12-02 20:09:55 +0100

    Continue training of existing layers.

--- main_w2v_keras_oneloss_512.py
+++ main_w2v_keras_oneloss_512.py
@@ -21,13 +21,13 @@ strPthIn = '/home/john/Dropbox/Harry_Potter/embedding/word2vec_data_all_books_e3
 
 # Path of npz file containing previously trained model's weights to load (if
 # None, new model is created):
-strPthMdl = '/home/john/Dropbox/Harry_Potter/lstm/512/20191127_090840/lstm_data.npz'
+strPthMdl = '/home/john/Dropbox/Harry_Potter/lstm/512/20191127_235916/lstm_data.npz'
 
 # Log directory (parent directory, new session directory will be created):
 strPthLog = '/home/john/Dropbox/Harry_Potter/lstm/512'
 
 # Learning rate:
-varLrnRte = 0.000001
+varLrnRte = 0.0000001
 
 # Number of training iterations over the input text:
 varNumItr = 3.0
@@ -45,7 +45,7 @@ lstNumNrn = [512, 512, 512, 512,
 # When loading pre-trained weights from disk, index of weights to asssign to
 # layer (e.g. to assign first item in list of loaded weights to first layer,
 # set first item to `0`). If `None`, do not assign pre-trained weights.
-lstLoadW = [0, 1, 2, None, -2, -1]
+lstLoadW = [0, 1, 2, 3, -2, -1]
 
 # Which layers are trainable?
 lstLyrTrn = [False, False, False, True, True, True]
